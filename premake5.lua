4bb0d6cbf2231f2b6ac71d74b98fb2e372675358
Keochori <69148826+Keochori@users.noreply.github.com>
2024-02-24 16:14:00 +0100

    Premake + updated ignore file

---
+++ premake5.lua
@@ -0,0 +1,29 @@
+workspace "Abaddon"
+    architecture "x64"
+
+    configurations
+    {
+        "Debug",
+        "Release"
+    }
+
+project "Abaddon"
+    location "Abaddon"
+    kind "WindowedApp"
+    language "C++"
+
+    defines {
+        "UNICODE"
+    }
+
+    files {
+        "%{prj.name}/src/**.cpp",
+        "%{prj.name}/src/**.h"
+    }
+
+    includedirs {
+        "%{prj.name}/src"
+    }
+
+    targetdir ("bin/%{cfg.buildcfg}")
+	objdir ("bin-int/%{cfg.buildcfg}")
